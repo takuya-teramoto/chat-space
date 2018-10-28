4d67f43ddfcf7ed6ccd669e96df530e650d4a7e1
takuya-teramoto <t.teramon23@gmail.com>
2018-10-28 18:19:13 +0900

    decide routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'chats#index'
-  resource :chats
+  resources :chat_groups, only: [:new, :create, :edit, :update] do
+    resources :chats, only: [:create, :index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
