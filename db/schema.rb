c55b5b3fd0662f0682e350550e538524bd74fdc6
takuya-teramoto <t.teramon23@gmail.com>
2018-10-28 16:04:30 +0900

    scaffold chat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_052933) do
+ActiveRecord::Schema.define(version: 2018_10_28_063501) do
 
   create_table "chat_groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
