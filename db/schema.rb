c8f37f8a3db255941113902f78471449cd0c84a8
takuya-teramoto <t.teramon23@gmail.com>
2018-10-28 14:23:45 +0900

    create model chat_group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_051805) do
+ActiveRecord::Schema.define(version: 2018_10_28_052056) do
+
+  create_table "chat_groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
