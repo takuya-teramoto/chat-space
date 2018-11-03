51f14b174a617f859adab40b69954c1ab4895746
takuya-teramoto <t.teramon23@gmail.com>
2018-11-03 14:44:14 +0900

    create chat_group controller

---
+++ app/controllers/chat_groups_controller.rb
@@ -0,0 +1,68 @@
+class ChatGroupsController < ApplicationController
+before_action :set_chat_group, only: [:show, :edit, :update, :destroy]
+before_action :move_to_index, except: [:index, :show]
+before_action :confirm_current_user, only: [:edit, :update, :destroy]
+
+
+  def new
+    @chat_group = ChatGroup.new
+  end
+
+  def create
+    @chat_group = ChatGroup.new(chat_group_params)
+    respond_to do |format|
+      if @chat_group.save
+        @user_chat_group = UserChatGroup.new(user_id: current_user.id, chat_group_id: @chat_group.id)
+        @user_chat_group.save
+        format.html { redirect_to chat_group_chats_path(@chat_group), notice: 'chat_group was successfully created.' }
+        format.json { render :show, status: :created, location: @chat_group }
+      else
+        format.html { render :new }
+        format.json { render json: @chat_group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @chat_group.update(chat_group_params)
+        format.html { redirect_to chat_group_chats_path(@chat_group), notice: 'Chat_group was successfully updated.' }
+        format.json { render :show, status: :ok, location: @chat_group }
+      else
+        format.html { render :edit }
+        format.json { render json: @chat_group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_chat_group
+    @chat_group = ChatGroup.find(params[:id])
+  end
+
+  def chat_group_params
+    params.require(:chat_group).permit(:name)
+  end
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
+
+  def confirm_current_user
+    redirect_to chat_group_chats_path(@chat_group) unless authenticate_user?(@chat_group)
+  end
+
+  def authenticate_user?(chat_group)
+    judge = false
+    chat_group.user_ids.each do |id|
+      if id == current_user.id
+        judge =  true
+      end
+    end
+    return judge
+  end
+end
