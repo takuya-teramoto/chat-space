aae892c386608328edb2dbdfcaaa524836415e07
takuya-teramoto <t.teramon23@gmail.com>
2018-11-03 17:00:36 +0900

    redirect to signin or signup when user isn't signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :move_to_sign_in, if: :devise_controller?, except: [:new, :create]
+  before_action :move_to_sign_in, unless: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def move_to_sign_in
+    redirect_to user_session_path unless user_signed_in?
   end
 end
