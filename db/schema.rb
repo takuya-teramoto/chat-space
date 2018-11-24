67746a3e5e29735aa639e86a267e7a3b4f87ee5b
takuya-teramoto <t.teramon23@gmail.com>
2018-11-24 17:47:08 +0900

    add index for :name in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_131321) do
+ActiveRecord::Schema.define(version: 2018_11_23_042204) do
 
   create_table "chat_groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2018_11_08_131321) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
