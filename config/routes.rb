ff874cf43b2647b66f9ff5d3eaeb55ae205ce922
takuya-teramoto <t.teramon23@gmail.com>
2018-11-24 18:00:28 +0900

    add route of 'search' for chat_group controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'chats#index'
   resources :chat_groups, only: [:new, :create, :edit, :update] do
     resources :chats, only: [:create, :index]
+    collection do
+      get 'search'
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
