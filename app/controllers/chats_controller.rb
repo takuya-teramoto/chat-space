56eb3c955a24d87400eaac86d2efafe966388bba
takuya-teramoto <t.teramon23@gmail.com>
2018-11-27 21:21:26 +0900

    change index action for ajax by introducing 'respond_to' method for json

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,36 +1,24 @@
 class ChatsController < ApplicationController
-  # before_action :set_chat, only: [:show, :edit, :update, :destroy]
-  # before_action :confirm_current_user, only: [:edit, :update, :destroy]
-  # GET /chats
-  # GET /chats.json
   def index
-    if params[:chat_group_id]
-      @chat = Chat.new
-      @chat_group = ChatGroup.find(params[:chat_group_id])
-      @chats = @chat_group.chats.order(id: "DESC").includes(:user)
+    respond_to do |format|
+      format.html{
+        if params[:chat_group_id]
+          @chat = Chat.new
+          @chat_group = ChatGroup.find(params[:chat_group_id])
+          @chats = @chat_group.chats.order(id: "DESC").includes(:user)
+        end
+        @chat_groups = ChatGroup.order(id: "DESC").includes(:chats)
+      }
+      format.json{
+        @newest_chats = Chat.where(chat_group_id: params[:chat_group_id]).where('id > ?', params[:chat_id]).includes(:user)
+      }
     end
-    @chat_groups = ChatGroup.all.order(id: "DESC").includes(:chats)
   end
 
-  # GET /chats/1
-  # GET /chats/1.json
-  # def show
-  # end
-
-  # GET /chats/new
-  # def new
-  #   @chat = Chat.new
-  # end
-
-  # GET /chats/1/edit
-  # def edit
-  # end
-
-  # POST /chats
-  # POST /chats.json
   def create
     @chat = Chat.new(chat_params)
-    @chats = Chat.all.order(id: "DESC").includes(:user)
+    @chat_group = ChatGroup.find(params[:chat_group_id])
+    @chats = @chat_group.chats.order(id: "DESC").includes(:user)
     if @chat.save
       render :create
     else
@@ -38,37 +26,8 @@ class ChatsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /chats/1
-  # PATCH/PUT /chats/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @chat.update(chat_params)
-  #       format.html { redirect_to @chat, notice: 'Chat was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @chat }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @chat.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /chats/1
-  # DELETE /chats/1.json
-  # def destroy
-  #   @chat.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to chats_url, notice: 'Chat was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def chat_params
       params.require(:chat).permit(:text, :image).merge(user_id: current_user.id, chat_group_id: params[:chat_group_id])
     end
-
-    # def confirm_current_user
-    #   redirect_to action: :index unless current_user.id == @chat.user.id
-    # end
 end
